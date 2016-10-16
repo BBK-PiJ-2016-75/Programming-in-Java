3802678c0c9df126773743c381396f05103f0830
Eliana Bayona <Eliana@Elianas-MacBook-Pro.local>
2016-10-16 23:59:02 +0100

    Added working Optimus Prime programme, and first attempt at Poker Hands.

---
+++ 01-11_poker_hands.groovy
@@ -0,0 +1,17 @@
+//Program that takes five cards from the user and then outputs the best poker hand that the user has.
+
+int card1 = 0;
+int card2 = 0;
+int card3 = 0;
+int card4 = 0;
+int card5 = 0;
+int heartTot = 0;
+int spadeTot = 0;
+int clubTot = 0;
+int diamTot = 0;
+
+print("Please enter your first card rank: ");
+card1 = Integer.parseInt(System.console().readLine());
+print("Now, please enter your first card suit: ");
+Str suit = System.console().readLine
+ 
---
+++ 01-17_optimus_prime.groovy
@@ -0,0 +1,35 @@
+//Program to find and print the nearest prime number to a user provided integer.
+
+print("Please enter a positive integer: ");
+int target = Integer.parseInt(System.console().readLine());
+boolean nearPos = false;
+boolean nearNeg = false;
+int posCount = target + 1;
+int negCount = target - 1;
+while (!nearPos) {
+	int denom = 2;
+	while (denom < posCount) {
+		if (posCount % denom == 0) {
+			posCount++;
+			denom = 2;
+		} else denom++;
+	}
+	nearPos = true;
+}
+while (!nearNeg) {
+	int denom = 2;
+	while (denom < negCount) {
+		if (negCount % denom == 0) {
+			negCount--;
+			denom = 2;
+		} else denom++;
+	}
+	nearNeg = true;
+}
+int posDiff = posCount - target;
+int negDiff = target - negCount;
+if (posDiff < negDiff) {
+	println("The nearest prime number is " + posCount + ".");
+} else if (negDiff < posDiff) {
+	println("The nearest prime number is " + negCount + ".");
+}else println("The nearest prime numbers are " + negCount + " and " + posCount + ".");
