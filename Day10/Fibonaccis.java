63637ec4d0df42bc2b78f7078918cf5874964ae5
Dennis Bueso-Calix <dlbcalix@gmail.com>
2016-12-05 19:59:21 +0000

    Day 10

---
+++ Day10/Fibonaccis.java
@@ -0,0 +1,29 @@
+public class Fibonaccis {
+  
+  public static int fibRecursive(int n) {
+    if ((n == 1) || (n == 2)) {
+      return 1;
+    } else {
+      int result = fibRecursive(n - 1) + fibRecursive(n - 2);
+      return result;
+    }
+  }
+  
+  public static int fibIterative(int n) {
+    int x = 1;
+    int y = 1;
+    for (int i = 1; i < n; i++) {
+      int temp = y;
+      y += x;
+      x = temp;
+    }
+    return x;
+  }
+  
+  public static void main(String[] args) {
+    int recursive = fibRecursive(9);
+    System.out.println(recursive);
+    int iterative = fibIterative(9);
+    System.out.println(iterative);
+  }
+}
