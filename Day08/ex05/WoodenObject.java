f11b1be6558d937c9e207bc955ea1861beb3e2f1
Dennis Bueso-Calix <dlbcalix@gmail.com>
2017-02-09 16:43:11 +0000

    Finished Day 08 exercises, including Checkstyle.

---
+++ Day08/ex05/WoodenObject.java
@@ -0,0 +1,12 @@
+package ex05;
+
+/**
+ * A wooden object can be burnt.
+ */
+public interface WoodenObject {
+
+  /**
+   * Burns the wooden object.
+   */
+  void burn();
+}
---
+++ Day08/ex06/Teacher.java
@@ -0,0 +1,20 @@
+package ex06;
+
+/**
+ * A teacher has only teaching responsibilities.
+ */
+public class Teacher {
+  private String name;
+
+  public Teacher(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void teach(String lessonName) {
+    System.out.println("Teaching lesson: " + lessonName);
+  }
+}
