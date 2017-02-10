a8c82f5c9cafd712ccbedebe06b0d05e6a8a77cd
Dennis Bueso-Calix <dlbcalix@gmail.com>
2017-02-10 17:35:24 +0000

    Completed up to Day09, Exercise02.

---
+++ Day09/ex01/Comparator.java
@@ -0,0 +1,23 @@
+package ex01;
+
+/**
+ * Compares two given objects and returns the one which has the highest value.
+ * The objects must be the same Comparable type.
+ */
+public class Comparator {
+
+  /**
+   * Returns whichever given Comparable object has the highest value.
+   * @param comparableOne The first Comparable object.
+   * @param comparableTwo The second Comparable object.
+   * @param <T> Ensures that the objects are Comparable.
+   * @return The Comparable object with the highest value.
+   */
+  public <T extends Comparable<T>> T getMax(T comparableOne, T comparableTwo) {
+    if (comparableOne.compareTo(comparableTwo) > 0) {
+      return comparableOne;
+    } else {
+      return comparableTwo;
+    }
+  }
+}
---
+++ Day09/ex01/ComparatorTester.java
@@ -0,0 +1,19 @@
+package ex01;
+
+/**
+ * Simple program to test the method of the Comparator class.
+ */
+public class ComparatorTester {
+
+  public static void main(String[] args) {
+    ComparatorTester tester = new ComparatorTester();
+    tester.run();
+  }
+
+  private void run() {
+    Comparator compare = new Comparator();
+    System.out.println("Max of 7 & 11: " + compare.getMax(7, 11));
+    System.out.println("Max of 9.3 & 1.4: " + compare.getMax(9.3, 1.4));
+    System.out.println("Max of \"12\" & \"35\": " + compare.getMax("12", "35"));
+  }
+}
